2eb1d08925a51c3c8f8ba19a711eb694d9ead989
santiago.londono <londono@informatik.uni-bonn.de>
2014-08-29 10:55:43 +0200

    Construction of the assessment page. Added header with Diachron logo to layout. Mixed HTML/CSS fixes.

---
+++ src/ontowiki-096/extensions/site/sites/diachron/js/quality-assessment.js
@@ -0,0 +1,23 @@
+/**
+ * Quality Assessment module. Provides the functionalities necessary to invoke the quality assessment process
+ * via the REST API. Implemented by following the JavaScript module pattern.
+ * Requires:
+ * 		- JQuery.
+ */
+var qualityAsmEngine = (function() {
+	
+	// Private attributes, implemented by mean of closures
+	var QUALITY_SERVICE_ENDPOINT = "";
+	
+	// Module instance, public attributes and methods
+	var thisObj = {};
+	
+	// Setup and start the quality assessment process
+	thisObj.launchQualityAssessment = function(dataSetURI, arrMetricURIs, boolGenReport) {
+		
+		
+	}
+		
+	return thisObj;
+	
+})();
